0011d8631598ea4260dc8f9e3cdc6b0373ed437c
Doug Hellmann <doug@doughellmann.com>
2015-08-25 21:05:43 +0000

    add list script

---
+++ bin/listnotes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Produce a list of notes, in the order they were added, by version.
+"""
+
+from __future__ import print_function
+
+import argparse
+import collections
+import os
+import re
+import subprocess
+
+TAG_PAT = re.compile('tag: ([\d\.]+)')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('relnotesdir', nargs='?', default='releasenotes')
+args = parser.parse_args()
+
+relnotesdir = args.relnotesdir
+notesdir = os.path.join(relnotesdir, 'notes')
+
+files_and_tags = collections.OrderedDict()
+
+# FIXME(dhellmann): This might need to be more line-oriented in the
+# production script.
+history_results = subprocess.check_output(
+    ['git', 'log', '--pretty=%x00%H %d', '--name-only', notesdir],
+)
+history = history_results.split('\x00')
+for i, h in enumerate(history):
+    h = h.strip()
+    if not h:
+        continue
+    print('-' * 30)
+    print(i)
+    hlines = h.splitlines()
+    tags = TAG_PAT.findall(hlines[0])
+    print(tags)
+    filenames = hlines[2:]
+    if not tags:
+        tags = ['untagged']
+    for t in tags:
+        files_and_tags.setdefault(t, []).extend(filenames)
+    print(filenames)
+    print('-' * 30)
+
+for t, filenames in files_and_tags.items():
+    print(t, filenames)
