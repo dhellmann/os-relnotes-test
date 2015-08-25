f4621016ec71928913f052ff8f2161323497fc75
Doug Hellmann <doug@doughellmann.com>
2015-08-25 21:41:34 +0000

    add slugs to filenames

--- bin/newnote.py
+++ bin/newnote.py
@@ -5,11 +5,14 @@ import os
 import uuid
 
 parser = argparse.ArgumentParser()
-parser.add_argument('relnotesdir', nargs='?', default='releasenotes')
+parser.add_argument('--relnotesdir', default='releasenotes')
+parser.add_argument('slug')
 args = parser.parse_args()
 
 relnotesdir = args.relnotesdir
 notesdir = os.path.join(relnotesdir, 'notes')
+
+slug = args.slug.replace(' ', '-')
 
 template = """\
 ---
@@ -25,7 +28,7 @@ other:
 
 for i in range(50):
     newid = str(uuid.uuid4())
-    notefilename = os.path.join(notesdir, newid + '.yaml')
+    notefilename = os.path.join(notesdir, '%s-%s.yaml' % (newid, slug))
     if not os.path.exists(notefilename):
         if not os.path.exists(notesdir):
             os.makedirs(notesdir)
