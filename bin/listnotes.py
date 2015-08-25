9de8196e8136ab1c1636a45c9d2e6157039cfb46
Doug Hellmann <doug@doughellmann.com>
2015-08-25 21:36:59 +0000

    track the earliest point we see a note, not the latest

--- bin/listnotes.py
+++ bin/listnotes.py
@@ -19,7 +19,8 @@ args = parser.parse_args()
 relnotesdir = args.relnotesdir
 notesdir = os.path.join(relnotesdir, 'notes')
 
-files_and_tags = collections.OrderedDict()
+versions = []
+earliest_seen = collections.OrderedDict()
 
 # Determine the current version, which might be an unreleased or dev
 # version.
@@ -31,25 +32,38 @@ history_results = subprocess.check_output(
     ['git', 'log', '--pretty=%x00%H %d', '--name-only', notesdir],
 )
 history = history_results.split('\x00')
-previous_version = current_version
+current_version = current_version
 for i, h in enumerate(history):
     h = h.strip()
     if not h:
         continue
     print('-' * 30)
-    print(i)
+    print(h)
+
     hlines = h.splitlines()
     tags = TAG_PAT.findall(hlines[0])
     print(tags)
     filenames = hlines[2:]
+
     if not tags:
-        tags = [previous_version]
+        tags = [current_version]
     else:
-        previous_version = tags[0]
-    for t in tags:
-        files_and_tags.setdefault(t, []).extend(filenames)
-    print(filenames)
-    print('-' * 30)
+        current_version = tags[0]
+
+    if current_version not in versions:
+        versions.append(current_version)
+
+    for f in filenames:
+        # Updated as older tags are found, handling edits to release
+        # notes.
+        earliest_seen[f] = tags[0]
+
+# Invert earliest_seen to make a list for each version.
+files_and_tags = collections.OrderedDict()
+for v in versions:
+    files_and_tags[v] = []
+for filename, version in earliest_seen.items():
+    files_and_tags[version].append(filename)
 
 for t, filenames in files_and_tags.items():
     print(t, filenames)
